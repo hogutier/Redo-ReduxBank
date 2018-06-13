36cf679ee13bc8545e79af5a7bd541b1dc758bb0
Horacio Gutierrez <hogutier@yahoo.com>
2018-06-13 17:44:57 -0500

    completed exercise with extra credit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-const balance = document.getElementById("balance");
-const deposit5 = document.getElementById("deposit5");
-const deposit25 = document.getElementById("deposit25");
-const withdraw5 = document.getElementById("withdraw5");
-const withdraw25 = document.getElementById("withdraw25");
+import { createStore } from 'redux'
 
-deposit5.onclick = () => console.log("Deposit 5");
-deposit25.onclick = () => console.log("Deposit 25");
-withdraw5.onclick = () => console.log("Withdraw 5");
-withdraw25.onclick = () => console.log("Withdraw 25");
+const balance = document.getElementById("balance");
+const deposit5 = document.getElementById("deposit5")
+const deposit25 = document.getElementById("deposit25")
+const withdraw5 = document.getElementById("withdraw5")
+const withdraw25 = document.getElementById("withdraw25")
+
+deposit5.onclick = () => store.dispatch({type: "deposit", amount: 5})
+deposit25.onclick = () => store.dispatch({type: "deposit", amount: 25})
+withdraw5.onclick = () => store.dispatch({type: "withdraw", amount: 5})
+withdraw25.onclick = () => store.dispatch({type: "withdraw", amount: 25})
+
+const reducer = (state = { balance: 0 }, action) => {
+  switch (action.type) {
+    case "deposit":
+      return { balance: state.balance + action.amount }
+    case "withdraw":
+      return { balance: state.balance - action.amount }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer)
+
+store.subscribe(() => {
+  console.log("The store state changed. Here's the new state: ", store.getState())
+  balance.innerText = `$ ${store.getState().balance}`
+})
